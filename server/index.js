4fe6a21a25970fd20a3bf3bbb63e9662e275b104
jaerius <rylynn1029@naver.com>
2023-08-01 02:30:22 +0900

    first

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ const port = 3042;
 app.use(cors());
 app.use(express.json());
 
-const balances = new Map([
-  ["B751181881B238BC03AFF2F05238523E5153CE7C", 100], // bob
-  ["2691DFF0EEAE64F2BF99B775A5E8A6BD0C1D01A3", 50], // alice
-  ["859769B97161E02C983A49D6B3DCCBECE32431E8", 75], // charles
-]);
+const balances = {
+  "0488b49f04c4d65cfa3cfdb12a9c2910c95fbf19d0b03e51ca3b17857d6fae000931d311475a918f2caa8f89b66ec71d5d663ad2a41714efe11a26ca8316ba507b" : 100,
+  "046266451ac09f2edca3b70cb11b62253caa151c60c2311bd32478897f0aca4bf7d701e30eb336879225f7a932b2352a52d56c7a70e7c85b006777798b3a1e5acd" : 50,
+  "04bd49e23dce7db94a23603bdd7c0d80c52a79670025ea7c0e18398323b1c7172845a706c6123d27c36c611375dd834e6ed06ba64b5fe3d1f6bd900f6396db4b0d" : 75,
+};
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
